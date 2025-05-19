e74e496381311710ee1dc0b1fce7d3d673a9aac8
Omkar Sunil Lolage <lolageomkar5@gmail.com>
2025-05-19 16:00:01 +0530

    resolves cors

--- Book_Store_using_Spring_React_and_MySQL/bookstore/src/main/java/com/example/bookstore/Controller/BookController.java
+++ Book_Store_using_Spring_React_and_MySQL/bookstore/src/main/java/com/example/bookstore/Controller/BookController.java
@@ -8,13 +8,14 @@ import com.example.bookstore.Repository.BookRepository;
 
 import java.util.List;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-
 @RestController
 @RequestMapping("/api")
+@CrossOrigin("http://localhost:5173")
 public class BookController {
 
     BookRepository bookRepository;
@@ -29,9 +30,9 @@ public class BookController {
     }
 
     @PostMapping("/books")
-    public String postMethodName(@RequestBody Book book) {       
+    public String postMethodName(@RequestBody Book book) {
         bookRepository.save(book);
         return "Insertion Successfull";
     }
-    
+
 }
--- Book_Store_using_Spring_React_and_MySQL/frontend/src/App.jsx
+++ Book_Store_using_Spring_React_and_MySQL/frontend/src/App.jsx
@@ -1,16 +1,16 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./Home";
-import Login from "./Login";
-import Register from "./Register";
+// import Home from "./Home";
+// import Login from "./Login";
+// import Register from "./Register";
 import Catalogue from "./Catalogue";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
+        {/* <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
+        <Route path="/register" element={<Register />} /> */}
         <Route path="/catalogue" element={<Catalogue />} />
       </Routes>
     </BrowserRouter>
