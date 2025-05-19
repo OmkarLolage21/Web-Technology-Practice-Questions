6bfbe2196743cf663a2989fe9a818cb3f4a9edd5
Omkar Sunil Lolage <lolageomkar5@gmail.com>
2025-05-19 18:30:36 +0530

    syntax similarity upgrade

--- Sem_Result_using_PHP_and_MySQL/backend.php
+++ Sem_Result_using_PHP_and_MySQL/backend.php
@@ -1,37 +1,33 @@
-    <?php
-        $host = "";
-        $username = "";
-        $password = "";
-        $db = "";
+<?php
+$mysqli = new mysqli("", "", "", "");
 
-        $conn = mysqli_connect($host,$username,$password,$db);
+if ($mysqli->connect_error) {
+  die("Connection failed: " . $mysqli->connect_error);
+}
 
-        $data = json_decode(file_get_contents("php://input"),true);
+$name = $_POST['name'];
+$roll = $_POST['roll_no'];
 
-        $stmt = $conn->prepare("INSERT INTO students (name, roll_no) VALUES (?,?)");
-        $stmt->bind_param("ss",$data["name"],$data["roll_no"]);
-        $stmt->execute();
-        $student_id = $stmt->insert_id;
+$mse = [$_POST['mse1'], $_POST['mse2'], $_POST['mse3'], $_POST['mse4']];
+$ese = [$_POST['ese1'], $_POST['ese2'], $_POST['ese3'], $_POST['ese4']];
 
-        $stmt = $conn->prepare("INSERT INTO mse (student_id, subject1, subject2, subject3, subject4) VALUES (?, ?, ?, ?, ?)");
-        $stmt->bind_param("iiiii", $student_id, $data["mse1"], $data["mse2"], $data["mse3"], $data["mse4"]);
-        $stmt->execute();
+$stmt = $mysqli->prepare("INSERT INTO students (name, roll_no) VALUES (?, ?)");
+$stmt->bind_param("ss", $name, $roll);
+$stmt->execute();
+$student_id = $stmt->insert_id;
 
-        $stmt = $conn->prepare("INSERT INTO ese (student_id, subject1, subject2, subject3, subject4) VALUES (?, ?, ?, ?, ?)");
-        $stmt->bind_param("iiiii", $student_id, $data["ese1"], $data["ese2"], $data["ese3"], $data["ese4"]);
-        $stmt->execute();
+$stmt = $mysqli->prepare("INSERT INTO mse (student_id, subject1, subject2, subject3, subject4) VALUES (?, ?, ?, ?, ?)");
+$stmt->bind_param("iiiii", $student_id, $mse[0], $mse[1], $mse[2], $mse[3]);
+$stmt->execute();
 
-        $totals = [];
-        for ($i = 1; $i <= 4; $i++) {
-            $mse = (int)$data["mse$i"];
-            $ese = (int)$data["ese$i"];
-            $total = $mse + $ese;
-            $totals[] = round($total, 2);
-        }
+$stmt = $mysqli->prepare("INSERT INTO ese (student_id, subject1, subject2, subject3, subject4) VALUES (?, ?, ?, ?, ?)");
+$stmt->bind_param("iiiii", $student_id, $ese[0], $ese[1], $ese[2], $ese[3]);
+$stmt->execute();
 
-        echo "<h5>Final Marks (30% MSE + 70% ESE)</h5><ul>";
-        for ($i = 0; $i < 4; $i++) {
-            echo "<li>Subject " . ($i+1) . ": " . $totals[$i] . "/100</li>";
-        }
-        echo "</ul>";
-    ?>
+echo "<h5>Final Marks (30% MSE + 70% ESE)</h5><ul>";
+for ($i = 0; $i < 4; $i++) {
+  $total = round($mse[$i] * 0.3 + $ese[$i] * 0.7, 2);
+  echo "<li>Subject " . ($i + 1) . ": $total / 100</li>";
+}
+echo "</ul>";
+?>
